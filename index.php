c235640c01c772383100a40f6203b1a517382a49
Leethium <dev@leethium.fr>
2016-05-05 11:31:19 +0200

    Init
    Signed-off-by: Leethium <dev@leethium.fr>

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DevZone</title>
+  <script src="/bower_components/angular/angular.js"></script>
+</head>
+<body>
+</body>
+</html>
---
+++ js/apps.js
@@ -0,0 +1,10 @@
+var app = angular.module('devZone', ['ngRoute', 'dndLists']);
+
+app.config(function($httpProvider) {
+	$httpProvider.defaults.headers.common.auth = _md5;
+});
+
+require("./angular/ctrl/devZone.js")(app);
+require("./angular/route/devZone.js")(app);
+require("./angular/directive/devZone.js")(app);
+require("./angular/filter/devZone.js")(app);
