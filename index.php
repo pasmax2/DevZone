d134724594baba75cdd35b9da8e542de51d7f653
Leethium <dev@leethium.fr>
2016-05-06 13:25:22 +0200

    Titres de colonnes, styles ...
    Signed-off-by: Leethium <dev@leethium.fr>

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
   <script src="js/materialize/bin/materialize.min.js"></script>
   <script src="js/compiled/angular.min.js"></script>
   <script src="js/compiled/apps-min.js"></script>
+  <link href="styles/angular_materialize.css" rel="stylesheet" type="text/css">
   <link href="styles/materialize.css" rel="stylesheet" type="text/css">
   <link href="styles/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <link href="styles/angular_materialize.css" rel="stylesheet" type="text/css">
   <link href="styles/main.css" rel="stylesheet" type="text/css">
 </head>
 <body>
@@ -30,68 +30,72 @@
   <div class="container">
     <div class="row">
       <div class="col s12 m3">
-        <div class="card grey darken-2">
+        <div class="card grey darken-2 card-stat-head">
           <div class="card-title center-align white-text">
             Suggestions
           </div>
         </div>
-        <div class="card teal darken-1">
+        <div class="card card-ticket teal darken-1">
           <div class="card-content white-text">
-            <div class="card-title">DevZone V2<a class="btn-floating grey darken-4 center-align waves-light right">+</a></div>
+            <div class="card-title">DevZone V2</div>
             <p>Le temps est venu de mettre à jour cet outil...</p>
           </div>
           <div class="card-action">
             <div class="chip">DevZone</div>
+            <a class="btn-floating grey darken-4 center-align waves-light right">+</a>
           </div>
         </div>
       </div>
       <div class="col s12 m3">
-        <div class="card grey darken-2">
+        <div class="card grey darken-2 card-stat-head">
           <div class="card-title center-align white-text">
             Approuvées
           </div>
         </div>
-        <div class="card grey darken-2">
+        <div class="card card-ticket grey darken-2">
             <div class="card-content white-text">
-              <div class="card-title">Crash caméra<a class="btn-floating grey darken-4 center-align waves-light right">+</a></div>
+              <div class="card-title">Crash caméra</div>
               <p>Mettre une caméra sur un joueur qui a déjà une caméra. <-> boucle.<br>
                 Anti afk avec la caméra</p>
             </div>
             <div class="card-action">
               <div class="chip">Crash</div>
+              <a class="btn-floating grey darken-4 center-align waves-light right">+</a>
             </div>
           </div>
       </div>
       <div class="col s12 m3">
-        <div class="card grey darken-2">
+        <div class="card grey darken-2 card-stat-head">
           <div class="card-title center-align white-text">
             En cours
           </div>
         </div>
-        <div class="card teal darken-1">
+        <div class="card card-ticket teal darken-1">
             <div class="card-content white-text">
-              <div class="card-title">DevZone V2<a class="btn-floating grey darken-4 center-align waves-light right">+</a></div>
+              <div class="card-title">DevZone V2</div>
               <p>Le temps est venu de mettre à jour cet outil...</p>
             </div>
             <div class="card-action">
               <div class="chip">DevZone</div>
+              <a class="btn-floating grey darken-4 center-align waves-light right">+</a>
             </div>
           </div>
       </div>
       <div class="col s12 m3">
-        <div class="card grey darken-2">
+        <div class="card grey darken-2 card-stat-head">
           <div class="card-title center-align white-text">
             Terminées
           </div>
         </div>
-        <div class="card green darken-1">
+        <div class="card card-ticket green darken-1">
             <div class="card-content white-text">
-              <div class="card-title">Mairie de quartier<a class="btn-floating grey darken-4 center-align waves-light right">+</a></div>
+              <div class="card-title">Mairie de quartier</div>
               <p>Je propose de remplacer l'Ikea par un grand parc, et au centre de ce parc : la mairie de Princeton.<br>
                 L'idée...</p>
             </div>
             <div class="card-action">
               <div class="chip">Map</div>
+              <a class="btn-floating grey darken-4 center-align waves-light right">+</a>
             </div>
           </div>
       </div>
